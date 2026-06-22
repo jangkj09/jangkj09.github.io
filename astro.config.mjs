444bf33a7dc63dec598de5970a729ba9639007f1
pompydev <hello@pompy.dev>
2026-06-22 19:08:48 +0900

    make fontawesome icons more type-safe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
+import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import emoji from "remark-emoji"
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   markdown: {
     remarkPlugins: [emoji],
   },
